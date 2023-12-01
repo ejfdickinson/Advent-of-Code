53591788c5ad9182e6523a630d21014f875e16e1
ejfdickinson <edmund@aboutenergy.co.uk>
2023-12-01 18:32:26 +0000

    Add Day 1b (horrible hacky approach)

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,21 +1,55 @@
+###
+
+# Horrible solution, first day back!
+
+###
+
 from string import digits
 
-def get_first_digit(s):
-    chars = [*s]
-    for char in chars:
-        if char in digits:
-            return int(char)
+SPELLED_DIGITS = ["one","two","three","four","five","six","seven","eight","nine"]
+MAP_SPELLED_DIGITS = { name: value for name, value in zip(SPELLED_DIGITS,range(1,10)) }
 
-def get_calibration_value(s):
-    first_digit = get_first_digit(s)
-    last_digit  = get_first_digit(reversed(s))
+def find_first_digit(s, check_for_spelled_digits=True):
+    for i in range(len(s)):
+        if s[i] in digits:
+            return int(s[i])
+        
+        if check_for_spelled_digits:
+            for k in MAP_SPELLED_DIGITS:
+                if s[i:].startswith(k):
+                    return MAP_SPELLED_DIGITS[k]
+
+def find_last_digit(s, check_for_spelled_digits=True):
+    n_chars = len(s)
+
+    for i in reversed(range(len(s))):
+        if s[i] in digits:
+            return int(s[i])
+        
+        if check_for_spelled_digits:
+            for k in MAP_SPELLED_DIGITS:
+                if s[:(i+1)].endswith(k):
+                    return MAP_SPELLED_DIGITS[k]
+
+def get_calibration_value(s, check_for_spelled_digits=True):
+    first_digit = find_first_digit(s, check_for_spelled_digits=check_for_spelled_digits)
+    last_digit  = find_last_digit (s, check_for_spelled_digits=check_for_spelled_digits)
     
-    return (10*first_digit + last_digit)    
+    return (10*first_digit + last_digit)
 
 def run(data):
-    calibration_values = [get_calibration_value(line) for line in data.splitlines()]
-    return sum(calibration_values)
+    calibration_values_raw = [
+        get_calibration_value(line, check_for_spelled_digits=False)
+        for line in data.splitlines()
+    ]
+    calibration_values_spelled = [
+        get_calibration_value(line, check_for_spelled_digits=True)
+        for line in data.splitlines()
+    ]
+    
+    return (sum(calibration_values_raw), sum(calibration_values_spelled))
 
+# Running script
 fp = "2023/inputs/day1"
 with open(fp) as file:
     data = file.read()
