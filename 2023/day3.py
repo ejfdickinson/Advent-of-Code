b88be3a4bd96859cfac7be89d9fc69385e3cc998
ejfdickinson <edmund@aboutenergy.co.uk>
2023-12-03 13:23:58 +0000

    Refactor adjacent coordinate determination

--- 2023/day3.py
+++ 2023/day3.py
@@ -1,48 +1,66 @@
+def get_coords_adj(x,y,xmax,ymax,length=1):
+    # Get list of surrounding adjacent coordinates to a (length,1) rectangle
+    # with origin x,y on a grid with bounds xmax,ymax
+    coords_adj = []
+
+    xend = x + length - 1
+
+    # Cardinal directions
+    if y > 0:
+        coords_adj += [(x+i,y-1) for i in range(length)]
+    if y < ymax:
+        coords_adj += [(x+i,y+1) for i in range(length)]
+    if x > 0:
+        coords_adj.append((x-1,y))
+    if xend < xmax:
+        coords_adj.append((xend+1,y))
+
+    # Diagonals
+    if y > 0:
+        if x > 0:
+            coords_adj.append((x-1,y-1))
+        if xend < xmax:
+            coords_adj.append((xend+1,y-1))
+    if y < ymax:
+        if x > 0:
+            coords_adj.append((x-1,y+1))
+        if xend < xmax:
+            coords_adj.append((xend+1,y+1))
+
+    return coords_adj
+
+class Gear:
+    def __init__(self, coord):
+        self.coord = coord
+
+    def get_nums_adj(self, charmap):
+        coords_adj = []
+        nums_adj = []
+
+
 class Number:
     def __init__(self, s, coord_end):
         self.val = int(s)
         # Calculate digit
         x,y = coord_end
-        self.coord_start = (x+1-len(s), y)
-        self.coord_end   = coord_end
+        self.length = len(s)
+        self.start = (x+1-self.length, y)
 
     def is_engine_part(self, charmap):
-        chars_adj = ''
-
-        xstart,y = self.coord_start
-        xend,_ = self.coord_end
-
-        xmax = len(charmap)-1
-        ymax = len(charmap[0])-1
-
-        # Adjacent chars (cardinal directions)
-        if y > 0:
-            chars_adj += ''.join(charmap[y-1][xstart:xend+1])
-        if y < ymax:
-            chars_adj += ''.join(charmap[y+1][xstart:xend+1])
-        if xstart > 0:
-            chars_adj += charmap[y][xstart-1]
-        if xend < xmax:
-            chars_adj += charmap[y][xend+1]
-
-        # Adjacent chars (diagonal directions)
-        if y > 0:
-            if xstart > 0:
-                chars_adj += charmap[y-1][xstart-1]
-            if xend < xmax:
-                chars_adj += charmap[y-1][xend+1]
-        if y < ymax:
-            if xstart > 0:
-                chars_adj += charmap[y+1][xstart-1]
-            if xend < xmax:
-                chars_adj += charmap[y+1][xend+1]
-
-        return any(c != '.' for c in chars_adj)       
+        xmax = len(charmap[0])-1
+        ymax = len(charmap)-1
+        
+        chars_adj = [
+            charmap[y][x]
+            for x,y in get_coords_adj(*self.start, xmax, ymax, length=self.length)
+        ]
+        
+        return any(c != '.' for c in chars_adj)
 
 def make_charmap(data):
     return [[*line] for line in data.splitlines()]
 
-def parse_schematic(charmap):
+def parse_schematic_numbers(charmap):
     str_number = ''
     for y,line in enumerate(charmap):
         for x,c in enumerate(line):
@@ -58,9 +76,18 @@ def parse_schematic(charmap):
             yield Number(str_number, (len(line)-1,y))
             str_number = ''
 
+def parse_schematic_gears(charmap):
+    for y,line in enumerate(charmap):
+        for x,c in enumerate(line):
+            if c == '*':
+                yield Gear((x,y))
+
 def run(data):
     charmap = make_charmap(data)
-    numbers = parse_schematic(charmap)
+
+    numbers = parse_schematic_numbers(charmap)
+    # gears = parse_schematic_gears(charmap)
+    
     engine_parts = [n for n in numbers if n.is_engine_part(charmap)]
 
     return sum([part.val for part in engine_parts])
