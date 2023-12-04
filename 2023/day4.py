7378b8ca6da33b75d3099fed0116c836862cc328
ejfdickinson <edmund@aboutenergy.co.uk>
2023-12-04 12:42:44 +0000

    Add Day 4b

--- 2023/day4.py
+++ 2023/day4.py
@@ -19,6 +19,22 @@ def get_score(n):
     else:
         return 0
 
+def get_n_cards(match_counts):
+    # Generate new cards in order, starting with one of each.
+    # Done very literally! Based on our knowing we never having to backtrack.
+    n_games = len(match_counts)
+    n_cards = [1] * n_games
+    for k,n in enumerate(match_counts):
+        for i in id_newcards(k, n, n_games):
+            # Edit in place - guaranteed that (i > k)
+            # n_k copies of card k already obtained
+            n_cards[i] += n_cards[k]
+    
+    return n_cards
+
+def id_newcards(base, n, maxval):
+    return range(base + 1, min(base + n + 1, maxval))
+
 def run(data):
     games = parse_scratchcards(data)
     match_counts = [
@@ -27,8 +43,12 @@ def run(data):
     ]
 
     point_scores = [get_score(n) for n in match_counts]
+    n_cards = get_n_cards(match_counts)
 
-    return sum(point_scores)
+    return (
+        sum(point_scores),
+        sum(n_cards)
+    )
 
 # Running script
 fp = "2023/inputs/day4"
