625533d2a1aee7d99d0eee187fb14b55af907720
ejfdickinson <edmund@aboutenergy.co.uk>
2023-12-04 12:42:23 +0000

    Refactor Day 4a
    As we know we never need detail on match contents themselves, just a count

--- 2023/day4.py
+++ 2023/day4.py
@@ -10,13 +10,23 @@ def parse_scratchcards(data):
 
     return games
 
-def get_matches(a, b):
-    return set(a).intersection(set(b))
+def count_matches(a, b):
+    return len(set(a).intersection(set(b)))
+
+def get_score(n):
+    if n > 0:
+        return (2 ** (n-1))
+    else:
+        return 0
 
 def run(data):
     games = parse_scratchcards(data)
-    matches_by_game = [get_matches(winning_numbers, card_numbers) for winning_numbers, card_numbers in games]
-    point_scores = [2 ** (len(match)-1) if len(match) > 0 else 0 for match in matches_by_game]
+    match_counts = [
+        count_matches(winning_numbers, card_numbers)
+        for winning_numbers, card_numbers in games
+    ]
+
+    point_scores = [get_score(n) for n in match_counts]
 
     return sum(point_scores)
 
