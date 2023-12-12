4ce6c4cc4235cf266a5babc05529eeb5d1e8c39a
ejfdickinson <edmund@aboutenergy.co.uk>
2023-12-12 11:05:20 +0000

    Add Day 11 (full)

--- 2023/day11.py
+++ 2023/day11.py
@@ -4,7 +4,7 @@ def parse_map(data):
             if s == '#':
                 yield (x,y)
 
-def expand(galaxies):
+def expand(galaxies, mul=2):
     xvals, yvals = zip(*galaxies)
     xmax = max(xvals)
     ymax = max(yvals)
@@ -14,27 +14,36 @@ def expand(galaxies):
 
     galaxies_expanded = [
         (
-            x + len([xgap for xgap in xgaps if xgap < x]),
-            y + len([ygap for ygap in ygaps if ygap < y])
+            x + (mul-1) * len([xgap for xgap in xgaps if xgap < x]),
+            y + (mul-1) * len([ygap for ygap in ygaps if ygap < y])
         ) for x,y in galaxies
     ]
 
     return galaxies_expanded
 
+def get_distances(galaxies):    
+    distances = {}
+    for i,galaxy1 in enumerate(galaxies):
+        for k,galaxy2 in enumerate(galaxies[i:]):
+            distances[(i,i+k)] = manhattan(*galaxy1, *galaxy2)
+
+    return distances
+
+
 def manhattan(x1,y1,x2,y2):
-    return abs(x2-x1) + abs(y2-y1)
+    return int(abs(x2-x1) + abs(y2-y1))
 
 def run(data):
     galaxies = list(parse_map(data))
-    galaxies_expanded = expand(galaxies)
+    galaxies_expanded = [
+        expand(galaxies, mul=mul) for mul in [1,1e6]
+    ]
 
-    distances = {}
-    for i,galaxy1 in enumerate(galaxies_expanded):
-        for k,galaxy2 in enumerate(galaxies_expanded[i:]):
-            distances[(i,i+k)] = manhattan(*galaxy1, *galaxy2)
+    distances = [get_distances(galaxies) for galaxies in galaxies_expanded]
 
     return (
-        sum(distances.values()),
+        sum(distances[0].values()),
+        sum(distances[1].values())
     )
 
 # Running script
